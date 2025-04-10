6df3d0f901124c7c0fc1dcad8f36c06be7137365
iMiebaka <miebaka.iwarri@datainfoproserv.com>
2025-04-10 14:38:01 +0100

    ✨ integrate NProgress for improved loading indicators during route changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
+import 'nprogress/nprogress.css'; 
+import "../styles/globals.css";
+
 import ReactQuery from "@/src/providers/ReactQuery";
 import ReduxProvider from "@/src/providers/ReduxProvider";
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
+import { Router } from "next/router";
+import NProgress from 'nprogress'; 
+
+
+
+Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done());  
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
