0d0660568dd7746c53f807cff4799e296aafede1
iMiebaka <miebaka.iwarri@datainfoproserv.com>
2025-04-11 07:13:28 +0100

    ✨ add publicId field to User model and implement user authentication method in UserRepository

--- src/repository/user.ts
+++ src/repository/user.ts
@@ -1,5 +1,7 @@
 import { Prisma, PrismaClient } from "../../generated/prisma/client";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import config from "../utils/config";
 
 const prisma = new PrismaClient();
 
@@ -19,5 +21,26 @@ export default class UserRepository {
     payload.password = await bcrypt.hash(payload.password, salt);
 
     await this.model.create({ data: { ...payload, profile: {} } });
+  }
+
+  async authenticateUser({ email, password }: any) {
+    const user = await this.getUser(email);
+    if (!user) throw { details: "User not found" };
+
+    const isPassword = await bcrypt.compare(password, user.password);
+    if (!isPassword) throw { details: "Username or password incorrect" };
+
+    const access_token = jwt.sign(
+      { publicId: user.publicId },
+      config.SECRET_KEY,
+      { expiresIn: "3d" }
+    );
+    const refresh_token = jwt.sign(
+      { publicId: user.publicId },
+      config.SECRET_KEY,
+      { expiresIn: "111d" }
+    );
+
+    return { access_token, refresh_token };
   }
 }
